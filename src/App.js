456812732308b467fbdc0ffe8650931621e1b918
rodrigo <rodrigo_coelho@orange.fr>
2022-04-19 19:07:17 +0200

    mobile navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar.js";
 
-const App = () => (
-  <div>
-    <Navbar />
-  </div>
-)
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,31 @@
 import styles from "./Navbar.module.scss";
-const { nav } = styles;
+import menuIcon from "../../images/icon-menu.svg";
+import logoImg from "../../images/logo.svg";
+import cartIcon from "../../images/icon-cart.svg";
+import avatarImg from "../../images/image-avatar.png";
 
-const Navbar = () => (
-  <nav className={nav}>Nav Content</nav>
-)
+const { nav, container, logoContainer, avatar } = styles;
+
+const Navbar = () => {
+    return (
+        <nav className={nav}>
+            <div className={container}>
+                <img src={menuIcon} alt="Menu Icon" />
+            </div>
+
+            <div className={[container, logoContainer].join(" ")}>
+                <img src={logoImg} alt="Logo" />
+            </div>
+
+            <div className={container}>
+                <img src={cartIcon} alt="Cart Icon" />
+            </div>
+
+            <div className={container}>
+                <img src={avatarImg} className={avatar} alt="Avatar" />
+            </div>
+        </nav>
+    )
+}
 
 export default Navbar;
