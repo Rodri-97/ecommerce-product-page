215d5017e258456224197d42483944509719a880
rodrigo <rodrigo_coelho@orange.fr>
2022-04-19 20:27:38 +0200

    expanded mobile menu done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,38 @@
 import styles from "./Navbar.module.scss";
+
 import menuIcon from "../../images/icon-menu.svg";
 import logoImg from "../../images/logo.svg";
 import cartIcon from "../../images/icon-cart.svg";
 import avatarImg from "../../images/image-avatar.png";
 
-const { nav, container, logoContainer, avatar } = styles;
+import { useState, useEffect } from "react";
+
+const { 
+        nav, container, logoContainer, avatar, hamburgerMenu, 
+        expandMenu, hideMenu, menuElement 
+} = styles;
 
 const Navbar = () => {
+    const maxMobileWidth = 800;
+    const [isMobile, setIsMobile] = useState(window.innerWidth < maxMobileWidth);
+
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            const isItMobile = window.innerWidth < maxMobileWidth;
+            if (isItMobile !== isMobile) setIsMobile(isItMobile);
+        });
+    });
+
+    const [displayMenu, setDisplayMenu] = useState(false);
+
     return (
         <nav className={nav}>
             <div className={container}>
-                <img src={menuIcon} alt="Menu Icon" />
+                <img src={menuIcon} 
+                     alt="Menu Icon" 
+                     className={hamburgerMenu} 
+                     onClick={() => setDisplayMenu(!displayMenu)}
+                />
             </div>
 
             <div className={[container, logoContainer].join(" ")}>
@@ -24,6 +46,14 @@ const Navbar = () => {
             <div className={container}>
                 <img src={avatarImg} className={avatar} alt="Avatar" />
             </div>
+
+            <ul className={(displayMenu && isMobile) ? expandMenu : hideMenu}>
+                <li className={menuElement}>Collections</li>
+                <li className={menuElement}>Men</li>
+                <li className={menuElement}>Women</li>
+                <li className={menuElement}>About</li>
+                <li className={menuElement}>Contact</li>
+            </ul>
         </nav>
     )
 }
